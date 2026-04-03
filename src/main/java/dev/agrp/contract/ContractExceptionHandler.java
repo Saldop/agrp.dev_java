38a0af3d6b3aef9f363e5876a9381bbfbae93c06
Adam Podlas <adam.podlas@gmail.com>
2026-04-03 10:38:58 +0200

    fix docker envs, add error logging

--- src/main/java/dev/agrp/contract/ContractExceptionHandler.java
+++ src/main/java/dev/agrp/contract/ContractExceptionHandler.java
@@ -1,5 +1,7 @@
 package dev.agrp.contract;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,12 +10,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice(basePackageClasses = ContractController.class)
 public class ContractExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(ContractExceptionHandler.class);
+
     @ExceptionHandler(ContractAnalysisException.class)
     public ResponseEntity<Void> handle(ContractAnalysisException e) {
         HttpStatus status = switch (e.getStage()) {
             case PDF_EXTRACTION -> HttpStatus.UNPROCESSABLE_ENTITY;
             default -> HttpStatus.BAD_GATEWAY;
         };
+        log.error("Contract analysis failed at stage {}: {}", e.getStage(), e.getMessage(), e);
         return ResponseEntity.status(status).build();
     }
 }
