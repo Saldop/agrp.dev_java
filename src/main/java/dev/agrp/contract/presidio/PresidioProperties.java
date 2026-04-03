47e48dbfe40a9dfdb70168c046658e9d255f2b8d
Adam Podlas <adam.podlas@gmail.com>
2026-04-03 14:50:27 +0200

    remove dead presidio anonymizer infrastructure

--- src/main/java/dev/agrp/contract/presidio/PresidioProperties.java
+++ src/main/java/dev/agrp/contract/presidio/PresidioProperties.java
@@ -3,4 +3,4 @@ package dev.agrp.contract.presidio;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "presidio")
-public record PresidioProperties(String analyzerUrl, String anonymizerUrl) {}
+public record PresidioProperties(String analyzerUrl) {}
